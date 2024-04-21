3b452802b348f7ed396eb096456eb89967b3b14e
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-04-21 17:29:03 +0200

    Somewhat works

--- DriverTest/Program.cs
+++ DriverTest/Program.cs
@@ -1,4 +1,5 @@
 using DriverTest.Models;
+using DriverTest.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IResultRepositories, ResultRepositories>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DriverTestContext>(options => options.UseSqlServer(connectionString));
 
